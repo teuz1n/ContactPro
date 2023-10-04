f2c57cdb05ec10dacb1de331cff6f4e889b3082d
matheusvogel <mulls4852@gmail.com>
2023-10-04 13:33:31 -0300

    Implementando autenticação com sanctum para register, login e logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/auth/register', [UserController::class, 'createUser']);
+Route::post('/auth/login', [UserController::class, 'loginUser']);
+Route::post('/auth/logout', [UserController::class, 'logoutUser'])->middleware('auth:sanctum');
